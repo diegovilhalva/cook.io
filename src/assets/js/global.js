6fb1fea9744cb5e13e6301331c5e6f3c97998d04
Diego Vilhalva <diegorocha450@yahoo.com>
2025-02-17 10:09:38 -0300

    tab section created in home page

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -1,7 +1,10 @@
-/**
- * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+
 
 "use strict";
+
+
+window.addEventOnElements= ($elements,eventType,callback) => {
+    for (const $element of $elements){
+        $element.addEventListener(eventType,callback)
+    }
+}
